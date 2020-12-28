de7c3d1814ada546ee2aef55224b3f067ebe4d3e
apple-yagi <applesryu@gmail.com>
2020-12-28 11:21:12 +0900

    update

--- pages/errors/404.vue
+++ pages/errors/404.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="text-center">
-    <img src="/hira_zonbi_walk.gif" alt="zonbi" />
+  <div class="not-found">
+    <h1 class="not-found_title">Not Found!</h1>
+    <img class="mx-auto" src="/hira_zonbi_walk.gif" alt="zonbi" />
   </div>
 </template>
 
@@ -8,3 +9,13 @@
 import Vue from 'vue'
 export default Vue.extend({})
 </script>
+
+<style lang="scss" scoped>
+.not-found {
+  text-align: center;
+  align-items: center;
+  &_title {
+    font-size: 26px;
+  }
+}
+</style>
